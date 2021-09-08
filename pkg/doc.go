e26998380c8519459c26fc3d82e0cf7bcebd9de0
Trajan0x <trajan0x@users.noreply.github.com>
2021-09-08 17:45:10 -0400

    add importable

---
+++ pkg/doc.go
@@ -0,0 +1,5 @@
+// Package pkg is an empty package that allows this package to be imported to avoid issues with unknown package while running go generate
+package pkg
+
+// Importable is a constant to make the package importable for go:generate commands
+const Importable = 0
